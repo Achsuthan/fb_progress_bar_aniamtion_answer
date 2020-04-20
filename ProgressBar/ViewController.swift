9eaa57f89f133a7357a9de2fc813edd8bebf3630
Achsuthan <achsuthan@icloud.com>
2020-04-20 20:47:45 +0530

    1. Code added

--- ProgressBar/ViewController.swift
+++ ProgressBar/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var btAnimate: UIButton!
+    
+    var higherValue:Float = 100
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.btAnimate.addTarget(self, action: #selector(self.btAnimate(_:)), for: .touchUpInside)
     }
-
-
+    
+    @IBAction func btAnimate(_: Any){
+        print("Start animate")
+        self.higherValue -= 5
+        if(self.higherValue > -5){
+            let floatValue: Float = Float(self.higherValue/Float(100))
+            self.progressBar.setProgress(floatValue, animated: true)
+        }
+    }
+    
+    
 }
 
